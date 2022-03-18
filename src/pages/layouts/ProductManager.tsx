caee58d9c8742d1d81d835e6872f5faeafe95a09
Duy Ha <havanduy@gmail.com>
2022-03-18 08:52:36 +0700

    feat: update

---
+++ src/pages/layouts/ProductManager.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+type Props = {}
+
+const ProductManager = (props: Props) => {
+    return(
+
+        <div>ProductManager</div>
+    )
+} 
+export default ProductManager
